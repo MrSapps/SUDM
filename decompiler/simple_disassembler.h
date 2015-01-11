928f511b218588d95a69693d53df798d27263cfa
Paul <Paul@paul.com>
2015-01-11 23:40:46 +0000

    try a c-style cast this time

--- decompiler/simple_disassembler.h
+++ decompiler/simple_disassembler.h
@@ -76,7 +76,7 @@ public:
 	}
 
 #define OPCODE_BASE(val) \
-	case static_cast<uint32>(val): \
+	case (uint32)val: \
 		full_opcode = (full_opcode << 8) + static_cast<uint32>(val);
 
 #define OPCODE_END break;
