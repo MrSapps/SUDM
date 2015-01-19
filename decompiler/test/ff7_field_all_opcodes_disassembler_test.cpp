e9716d05e40597218ac74973efbba94e7a3dcca7
Paul <Paul@paul.com>
2015-01-19 20:33:57 +0000

    Add unit test file

---
+++ decompiler/test/ff7_field_all_opcodes_disassembler_test.cpp
@@ -0,0 +1,37 @@
+#include <gmock/gmock.h>
+#include "decompiler/ff7_field/ff7_field_disassembler.h"
+#include "decompiler/ff7_field/ff7_field_engine.h"
+#include "decompiler/ff7_field/ff7_field_codegen.h"
+#include "control_flow.h"
+#include "util.h"
+#include "make_unique.h"
+
+// Remove DISABLED_ to make it execute
+TEST(FF7Field, DISABLED_AllOpcodesDisassembler)
+{
+    InstVec insts;
+    FF7::FF7FieldEngine engine;
+
+    auto d = engine.getDisassembler(insts);
+    d->open("decompiler/test/ff7_all_opcodes.dat");
+    d->disassemble();
+    d->dumpDisassembly(std::cout);
+    std::cout << std::endl;
+
+    auto c = std::make_unique<ControlFlow>(insts, engine);
+    c->createGroups();
+
+    Graph g = c->analyze();
+    engine.postCFG(insts, g);
+
+    onullstream ns;
+    auto cg = engine.getCodeGenerator(std::cout);
+    cg->generate(insts, g);
+
+    ASSERT_EQ(insts[0]->_opcode, FF7::eOpcodes::RET);
+    ASSERT_EQ(insts[0]->_params.size(), 0);
+
+    // If it had a value then check the values using:
+    //ASSERT_EQ(insts[0]->_params[0]->getSigned(), 1);
+
+}
